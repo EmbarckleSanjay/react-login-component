f86eb5ceabb4c070b0168135b81dc43573b0a58e
sanjay <sanjay.k@embarckle.com>
2021-04-02 11:40:02 +0530

    Third!

--- src/config/env/development.ts
+++ src/config/env/development.ts
@@ -1,7 +1,7 @@
 import { IENVConfig } from 'types/envConfig';
 
 const settings: IENVConfig = {
-  apiBaseURL: 'http://192.168.43.196:3000',
+  apiBaseURL: 'http://192.168.29.196:3000',
   apiVersionPath: '/login',
   siteUrl: 'http://localhost:3000/',
   mode: 'localdev',
